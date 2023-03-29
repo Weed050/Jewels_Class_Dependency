3a7bfc725d786b24f7bac0adfea8b74bc613c66e
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-03-29 20:39:26 +0200

    Add project files.

---
+++ Jewels/Safe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jewels
+{
+    internal class Safe
+    {
+        private Jewels contents =  new Jewels();
+        private string safeCombination = "12345";
+        public Jewels Open(string combination)
+        {
+
+        }
+    }
+}
