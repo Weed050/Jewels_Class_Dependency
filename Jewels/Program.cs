214684c1792bb774dc34eebff1ed15a0a9315bba
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-03-29 21:07:20 +0200

    program is ready

--- Jewels/Program.cs
+++ Jewels/Program.cs
@@ -1,1 +1,11 @@
 ﻿//Console.WriteLine("Hello, World!");
+using Jewels;
+
+static void Main()
+{
+    Owner owner = new Owner(); 
+    Safe safe = new Safe();
+    JewelThief jewelThief = new JewelThief();    
+    jewelThief.OpenSafe(safe,owner);
+    Console.ReadKey();
+}
