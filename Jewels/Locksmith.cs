d833e4b9ca4180adb41f649a819bdc12d0509db4
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-03-29 21:00:27 +0200

    new class called locksmith, next will be thef which is evil locksmith (on the dark side)

--- Jewels/Locksmith.cs
+++ Jewels/Locksmith.cs
@@ -8,6 +8,20 @@ namespace Jewels
 {
     internal class Locksmith
     {
-
+        public void OpenSafe(Safe safe,Owner owner)
+        {
+            safe.PickLock(this);
+            Jewels safeContents = safe.Open(writeDownCombination);
+            ReturnContents(safeContents, owner);
+        }
+        private string writeDownCombination = null;
+        public void WriteDownCombination(string combination)
+        {
+            writeDownCombination = combination;
+        }
+        public void ReturnContents(Jewels safeContents,Owner owner)
+        {
+            owner.ReceiveContents(safeContents);
+        }
     }
 }
