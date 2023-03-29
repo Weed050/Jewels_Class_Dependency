7ff8c4958138685d093754a5082653357e828680
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-03-29 21:21:02 +0200

    method open safe used by thief which inherits from class locksmith return jelelery instead of robbing, because overriting method in class thief is messed up

--- Jewels/Program.cs
+++ Jewels/Program.cs
@@ -1,11 +1,8 @@
 ﻿//Console.WriteLine("Hello, World!");
 using Jewels;
 
-static void Main()
-{
     Owner owner = new Owner(); 
     Safe safe = new Safe();
     JewelThief jewelThief = new JewelThief();    
     jewelThief.OpenSafe(safe,owner);
     Console.ReadKey();
-}
