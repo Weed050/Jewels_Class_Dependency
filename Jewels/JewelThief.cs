9a33abd2b88bf22661d53a381ae711b823feec88
Weed050 <84383140+Weed050@users.noreply.github.com>
2023-03-29 21:04:58 +0200

    added class JewelThief

--- Jewels/JewelThief.cs
+++ Jewels/JewelThief.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Jewels
 {
-    internal class JewelThief
+    internal class JewelThief : Locksmith
     {
+        private Jewels stolenJewels = null;
+        public void ReturnContents(Jewels safeContents,Owner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine("Kradnę zawartość sejfu! " + stolenJewels.Sparkle());
+        }
     }
 }
